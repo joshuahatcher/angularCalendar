8da1be76890643dee2e0784ce1b41e9675f3744f
Joshua Hatcher <josh@ordergroove.com>
2015-01-09 18:01:15 -0500

    Rework logic of app to be simpler, less dependent on globals, eliminate some new Date calls, provide comments

--- js/main.js
+++ js/main.js
@@ -1,81 +1,88 @@
 var app = angular.module('calendar', []);
 
 app.controller('calendarCtrl', function($scope, $http) {
-    $scope.currDate = $scope.today = new Date();
+    var monthArr;
 
-    $scope.monthChange = function monthChange(val) {
-        $scope.currDate = val === -1 ? $scope.lastMonth : new Date($scope.year, $scope.month + val);
-        $scope.setNewMonth()
-    }
-
-    $scope.reset = function reset() {
-        $scope.currDate = $scope.today;
-        $scope.setNewMonth();
-    }
-
-    $scope.setNewMonth = function setNewMonth() {
-        $scope.firstDate = new Date($scope.currDate.getFullYear(), $scope.currDate.getMonth(), 1)
-        $scope.month = $scope.currDate.getMonth();
-        $scope.year = $scope.currDate.getFullYear();
-        $scope.monthDays = $scope.setDays($scope.year, $scope.month);
-    }
-
-    $scope.setDays = function setDays(year, month) {
-        var i, lastDays,
-            x = 1,
-            numDays = new Date(year, month + 1, 0).getDate(),
-            dayArr = [],
-            today = $scope.currDate.getMonth() === $scope.today.getMonth() &&
-                $scope.currDate.getFullYear() === $scope.today.getFullYear();
-
-        $scope.lastMonth = new Date(year, month, 0);
-
-        for (i = 1; i <= numDays; i++) {
-            dayArr.push({
-                day: i,
-                type: today && i === $scope.today.getDate() ? 'today' : 'current'
-            });
-        }
-
-        dayArr = $scope.setLastDays(dayArr, $scope.firstDate.getDay(), $scope.lastMonth.getDate());
-
-        return dayArr;
-    }
-
-    $scope.setLastDays = function setLastDays(arr, firstDay, lastDays) {
-        var i;
-
-        for (i = 0; i < firstDay; i++) {
-            arr.unshift({
-                day: lastDays,
-                type: 'last'
-            });
-            lastDays--;
-        }
-
-        return $scope.setNextDays(arr);;
-    }
-
-    $scope.setNextDays = function setNextDays(arr) {
-        var i,
-            x = 1;
-
-        // To keep all months formatted the same, preview maximum number of extra cells for next month.
-        for (i = arr.length; i < 42; i++) {
-            arr.push({
-                day: x,
-                type: 'last'
-            });
-            x++;
-        }
-        return arr;
-    }
-
-    // Init the current month params
-    $scope.setNewMonth();
-
+    // Grab data from JSON file for use in month/day name labels.
     $http.get('data.json')
         .success(function(data) {
-            $scope.dateObj = data;
+            $scope.daysOfWeek = data.days;
+            monthArr = data.months;
+            $scope.changeMonth();
         });
+
+    /**
+     * Get a date object for use in an adjacent month
+     * @method changeMonth
+     * @param {number} val Number by which to alter current month
+     * @param {number} yr Current year of date from which to alter current month
+     * @param {number} mo Current month of date from which to alter current month
+     * @param {boolean} innerCall Implies whether call was made via UI or privately in another function
+     */
+    $scope.changeMonth = function(val, yr, mo, innerCall) {
+        var dateTarget = innerCall ? 0 : 1,
+            // If fn is called without arguments, reset the calendar.
+            newCurr = val ? new Date(yr, monthArr.indexOf(mo) + val, dateTarget) : new Date();
+        if (innerCall) {
+            return newCurr;
+        }
+        setMonth(newCurr);
+    }
+
+    /**
+     * Sets new values to $scope date properties
+     * @method setMonth
+     * @param {Date} date Updated date from which to extract properties
+     */
+    function setMonth(date) {
+        $scope.yr = date.getFullYear();
+        $scope.mo = monthArr[date.getMonth()];
+        $scope.days = getDays(date);
+    }
+
+    /**
+     * Prepares numbers corresponding to dates to be shown in the UI
+     * @method getDays
+     * @param {Date} date Used to extract individual dates to be shown
+     * @return {Array}
+     */
+    function getDays(date) {
+        var i, len, lastMo, nextMo,
+            dayArr = [],
+            mo = date.getMonth(),
+            yr = date.getYear(),
+            numDays = new Date(yr, mo + 1, 0).getDate(),
+            // If this is being called via the initialization of the app,
+            // date will be equal to the current date, not necessarily the first of the month.
+            // This will avoid having to call new Date another time.
+            firstDay = date.getDate() > 1 ? date.getDay() + 1 - (date.getDate() % 7) : date.getDay();
+
+        for (i = 1, len = numDays; i <= numDays; i++) {
+            dayArr.push({
+                num: i,
+                type: 'current'
+            });
+        }
+        // Prepend current month dates with days from previous month to complete cells for first week
+        if (firstDay > 0) {
+            lastMo = $scope.changeMonth(-1, yr, mo, true).getDate();
+            for (i = 0; i < firstDay; i++) {
+                dayArr.unshift({
+                    num: lastMo,
+                    type: 'other'
+                });
+                lastMo--;
+            }
+        }
+        // Append current month dates with days from next month so all months have equal number of cells
+        if (dayArr.length < 42) {
+            for (i = dayArr.length; i < 42; i++) {
+                dayArr.push({
+                    num: i,
+                    type: 'other'
+                })
+            }
+        }
+        return dayArr;
+    }
 });
